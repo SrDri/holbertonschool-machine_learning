704a58bc98213e5056b894bfc2bf750c07c03aec
SrDri <juanjcarabali@gmail.com>
2021-12-02 06:54:22 +0700

    Update task and fix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-""" Size """
+""" Size matrix """
 
 
 def matrix_shape(matrix):
     result = []
     try:
-        while len(matrix) > 0:
-            result.append(len(matrix))
+        result.append(len(matrix))
+        while type(matrix[0]) != int:
+            result.append(len(matrix[0]))
             matrix = matrix[0]
     except TypeError:
         pass
