6a2925a5a430eb2105ac79a7099a98c7f2e16071
SrDri <juanjcarabali@gmail.com>
2021-12-02 06:59:55 +0700

    Update and fix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -3,9 +3,13 @@
 
 
 def matrix_shape(matrix):
+    """ Fix """
     result = []
-    result.append(len(matrix))
-    while type(matrix[0]) != int:
-        result.append(len(matrix[0]))
-        matrix = matrix[0]
+    try:
+        result.append(len(matrix))
+        while type(matrix[0]) != int:
+            result.append(len(matrix[0]))
+            matrix = matrix[0]
+    except TypeError:
+        pass
     return result
