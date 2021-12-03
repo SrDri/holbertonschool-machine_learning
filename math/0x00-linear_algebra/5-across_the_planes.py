85d2ee9483c2bc2b2e434840658f58fc08f260f6
SrDri <juanjcarabali@gmail.com>
2021-12-03 09:21:20 +0700

    Update task

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Add 2D matrix"""
+
+
+def add_matrices2D(mat1, mat2):
+    """2D add matrix"""
+    if len(mat1) == len(mat2):
+        result = []
+        for i in range(len(mat1)):
+            aux = []
+            if len(mat1[i]) == len(mat2[i]):
+                for j in range(len(mat1[i])):
+                    aux.append(mat1[i][j] + mat2[i][j])
+                result.append(aux)
+            else:
+                return None
